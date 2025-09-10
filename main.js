c284ac2cceb89b4476bc7ae4d0824931d2add675
K3yZz <1044094@oside.us>
2025-09-10 17:10:22 +0000

    Bug fixes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { drawPlayer, drawPlayerHealthBar, drawTimer, player, playerStats, scaleB
 import { checkCollision, checkCollisionWithMoney } from "./collision.js";
 import { startIntervals, stopIntervals } from "./interval.js";
 import { loadBackgroundCanvas, loadDeathOverlay } from "./UI.js";
-import { editBox, startButton, toSkillTreeButton, againButton, earningsBox } from "./skilltree.js";
+import { editBox, startButton, toSkillTreeButton, againButton, earningsBox, moneyBox } from "./skilltree.js";
 
 //*load backdrop
 loadBackgroundCanvas();
@@ -73,6 +73,7 @@ export function startGame() {
     editBox("delete", toSkillTreeButton);
     editBox("delete", againButton);
     editBox("delete", earningsBox);
+    editBox("delete", moneyBox)
     gameLoop();
 }
 
